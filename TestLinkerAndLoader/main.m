fda6c51fc0559656a0ea1568f889524d957995ba
xueshiqiao <xueshiqiao@tencent.com>
2022-07-03 01:26:51 +0800

    Initial Commit

---
+++ TestLinkerAndLoader/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestLinkerAndLoader
+//
+//  Created by XueshiQiao on 2022/7/3.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+  @autoreleasepool {
+      // insert code here...
+      NSLog(@"Hello, World!");
+  }
+  return 0;
+}
